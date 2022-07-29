aa9c52c02ec1bd8ddeaadd5f63c93d001f93aab6
obetta1 <obettabenjamin44@gmail.com>
2022-07-29 06:18:06 +0100

    pull conflict resolve

--- app/src/main/java/com/decagon/decafit/common/authentication/LoginFragment.kt
+++ app/src/main/java/com/decagon/decafit/common/authentication/LoginFragment.kt
@@ -49,7 +49,6 @@ class LoginFragment : Fragment() {
             if (Validation.validateEmailInput(email)) {
                 if (Validation.validatePasswordPattern(password)) {
                     findNavController().navigate(R.id.action_loginFragment_to_inputExerciseFragment)
-
                 } else {
                     // call for incorrect password here
                     snackBar("Invalid Password")
