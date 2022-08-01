4051dccb9db6e3272e419cc93ff38699ed37c053
Victor <86411135+victorbamikole@users.noreply.github.com>
2022-08-01 20:40:48 +0100

    Victor ft signup screen (#4)
    * created login screen
    
    * created login screen
    
    * sign up screen
    
    * signup screen
    
    * Test implementation
    
    Co-authored-by: obetta benjamin <86382174+obetta1@users.noreply.github.com>
    Co-authored-by: obetta1 <obettabenjamin44@gmail.com>

--- app/src/main/java/com/decagon/decafit/common/utils/Validation.kt
+++ app/src/main/java/com/decagon/decafit/common/utils/Validation.kt
@@ -1,6 +1,13 @@
 package com.decagon.decafit.common.utils
 
+import com.decagon.decafit.common.SignUpRequest
+
 object Validation {
+    val DIGITCHARACTER = Regex("[0-9]")
+    val SPECAILCHARAACTERS = Regex("[@#\$%^&+=*_-]")
+    val UPPERCASE = Regex("[A-Z]")
+    val LOWERCASE = Regex("[a-z]")
+
     fun validateEmailInput(email: String): Boolean {
         if (email.isEmpty() || !email.matches(EMAIL_PATTERN)) {
             return false
@@ -17,6 +24,7 @@ object Validation {
                 ")+"
     )
 
+
     //Validate user email pattern
     fun validatePasswordPattern(usersPassword: String): Boolean {
         val passwordPattern =
@@ -25,5 +33,67 @@ object Validation {
             return true
         }
         return false
+    }
+
+    //Validate full name input
+    fun validateFullNameInput(name: String): List<String> {
+        val result = mutableListOf<String>()
+        if (name.contains(DIGITCHARACTER)) {
+            result.add("Can't start with numbers")
+        }
+
+        if (name.contains(SPECAILCHARAACTERS)) {
+            result.add("must not contain special characters")
+        }
+        return result
+    }
+
+    // phone_number inputField validation
+    fun validatePhoneNumber(phone_number : String):Boolean{
+        return phone_number.length < 10
+    }
+
+    fun validateAccountData(accountData: SignUpRequest):List<String>{
+        val result = mutableListOf<String>()
+        val splitName = accountData.fullName.split(" ")
+        // check if the full name is entered and is a valid name
+        if(splitName.size <2 || splitName.size >3){
+            result.add("Enter your full name")
+        }else if (!validateEmailInput(accountData.email) ) {
+            result.add("cant be empty")
+        } else if ( validatePhoneNumber(accountData.phone_number)){
+            result.add("Incomplete number")
+        }
+        return result
+    }
+
+    fun validateUserProfileInput(user: SignUpRequest): Boolean {
+        return (user.fullName.isEmpty() || user.email.isEmpty() || validatePhoneNumber(user.phone_number) || !validateEmailInput(
+            user.password
+        ))
+    }
+    // passwordInputField validation
+    fun validatePasswordErrors(
+        password: String,
+    ): List<String> {
+
+        val result = mutableListOf<String>()
+
+        if (password.length <= 7) {
+            result.add("* Minimum of 8 characters")
+        }
+        if (!password.contains(UPPERCASE) || !password.contains(LOWERCASE)
+        ) {
+            result.add("* Uppercase and lowercase")
+        }
+        if (!password.contains(DIGITCHARACTER)) {
+            result.add("* Numbers")
+        }
+
+        if (!password.contains(SPECAILCHARAACTERS)) {
+            result.add("* Special characters")
+        }
+
+        return result
     }
 }
--- app/src/test/java/com/decagon/decafit/common/utils/ValidationTest.kt
+++ app/src/test/java/com/decagon/decafit/common/utils/ValidationTest.kt
@@ -1,10 +1,13 @@
 package com.decagon.decafit.common.utils
 
+import com.decagon.decafit.common.SignUpRequest
 import org.junit.Assert
 import org.junit.Test
 
 class ValidationTest {
     val email = "obettabenjamin44@gmil.com"
+    val phone_numberLeasThan9 = "070123"
+    val passwordLessThan8character = "Pas1#"
 
     @Test
     fun validatePasswordPattern() {
@@ -50,6 +53,45 @@ class ValidationTest {
     fun validate_empty_email_returns_false() {
         val expected = false
         val actual = Validation.validateEmailInput("")
+        Assert.assertEquals(expected, actual)
+    }
+
+    val fullName = "Victor"
+    val emailAddress= "victor@gmail.com"
+    val phone_number = "+2347056"
+    val password = "Manchester1"
+    @Test
+    fun  test_validateUserInput_wrongInput_returns_true(){
+        val expected = true
+        val actual = Validation.validateUserProfileInput(SignUpRequest(fullName, password, emailAddress, phone_number))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun validate_fullName_with_spacial_character() {
+        val expected = listOf("must not contain special characters")
+        val actual = Validation.validateFullNameInput("vic@@##")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun validate_fullName_starts_with_digiit_returns_cannot_start_with_digit() {
+        val expected = listOf("Can't start with numbers")
+        val actual = Validation.validateFullNameInput("123gghh")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun validate_phone_numberLessThan_9_digits_returns_true() {
+        val actual = Validation.validatePhoneNumber(phone_numberLeasThan9)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun validate_Password_input_lessThan_8_returns_Minimum_of_8_characters() {
+
+        val expected = listOf("* Minimum of 8 characters")
+        val actual = Validation.validatePasswordErrors(passwordLessThan8character)
         Assert.assertEquals(expected, actual)
     }
 }
