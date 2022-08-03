2d75c0b7dd70d42693bb55974facc09b518ef518
obetta1 <obettabenjamin44@gmail.com>
2022-08-03 23:12:38 +0100

    updated workout breakdown screen

--- app/src/main/java/com/decagon/decafit/common/utils/dommyData/WorkoutDatas.kt
+++ app/src/main/java/com/decagon/decafit/common/utils/dommyData/WorkoutDatas.kt
@@ -4,8 +4,8 @@ import com.decagon.decafit.workout.data.WorkoutItems
 
 val workoutData = listOf<WorkoutItems>(
 
-    WorkoutItems("Jumping jacks", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem amtLorem ipsum dolor sit amtLorem ip amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amt sum dolor sit amtLorem ipsum dolor sit amt  ipsum dolor sit amtLorem ipsum dolor sit amt ","1:00", 100),
-    WorkoutItems("Push-ups", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amt ","1:00", 45),
+    WorkoutItems("Jumping jacks", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem amtLorem ipsum dolor sit amtLorem ip amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amt sum dolor sit amtLorem ipsum dolor sit amt  ipsum dolor sit amtLorem ipsum dolor sit amt ","1:00", 0),
+    WorkoutItems("Push-ups", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amt ","1:00", 0),
     WorkoutItems("Knee push-ups", "Lorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum do amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amt lor sit amtLorem ipsum dolor sit amt  ","1:00", 0),
     WorkoutItems("Incline push-ups", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ips amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amt um dolor sit amt ","1:00", 0),
     WorkoutItems("Planks", "Lorem ipsum dolor sit amt Lorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ip amtLorem ipsum dolor sit amtLorem ipsum dolor sit amtLorem ipsum dolor sit amt sum dolor sit amtLorem ipsum dolor sit amt ","1:00", 0),
