00b5e8fee4860c1bdf3e01130a48ec4718766e6f
victorbamikole <victorbamikole92@gmail.com>
2022-08-20 18:37:12 +0100

    Dashboard Implementation

---
+++ app/src/main/java/com/decagon/decafit/common/dashboard/DashBoardAdapter.kt
@@ -0,0 +1,25 @@
+package com.decagon.decafit.common.dashboard
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DashBoardAdapter(): RecyclerView.Adapter<DashBoardAdapter.AppHolder>() {
+
+    inner class AppHolder(binding: RecyclerItemBinding): RecyclerView.ViewHolder(binding.root)  {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AppHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+
